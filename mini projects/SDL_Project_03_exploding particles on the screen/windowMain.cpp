0ee24c25cbfaa735c6f3c0a3c05775e35894ad10
Ed Amutete <53301147+ed-learner@users.noreply.github.com>
2020-08-28 16:41:33 +0200

    Update windowMain.cpp

--- mini projects/SDL_Project_03_exploding particles on the screen/windowMain.cpp
+++ mini projects/SDL_Project_03_exploding particles on the screen/windowMain.cpp
@@ -2,33 +2,42 @@
 #include <SDL.h>
 #include <math.h>
 #include "window.h"
+#include "particle.h"
+#include "swarm.h"
 
 using namespace std;
 
 int main(int argc, char* argv[]) {
 
+	double windowWidthCenter = Window::SCREEN_WIDTH /2;
+	double windowHeightCenter = Window::SCREEN_HEIGHT /2;
+
 	Window screen;
+	swarm swm;
 
 	if (screen.init() == false) {
 		cout << "Error initializing SDL" << endl;
 	}
 
 	while (true) {
-
-		//set color values
 		int elapsed = SDL_GetTicks();
+		screen.clear();
+		swm.update(elapsed);
+	
 		unsigned char red   = (unsigned char)(1 + sin(elapsed * 0.0001)) * 128;
 		unsigned char green = (unsigned char)(1 + sin(elapsed * 0.0004)) * 128;
 		unsigned char blue  = (unsigned char)(1 + sin(elapsed * 0.0008)) * 128;
 
-		//set window pixels
-		for (int y = 0; y < Window::SCREEN_HEIGHT; y++) {
-			for (int x = 0; x < Window::SCREEN_WIDTH; x++) {
-				screen.setWindowPixel(x, y, red, green, blue);				
-			}
+		const Particle* const pParticles = swm.getParticle();
+
+		for (int i = 0; i <swarm::numberOfParticles; i++) {
+			Particle particle = pParticles[i];
+
+			int x = (particle.pos_x + 1) * windowWidthCenter;
+			int y = (particle.pos_y * windowWidthCenter)+ windowHeightCenter;
+
+			screen.setWindowPixel(x, y, red, green, blue);
 		}
-	
-		//screen.setPixel(400, 300, 0, 0, 255); //set single pixel
 
 		//update the window
 		screen.updateWindow();
@@ -37,6 +46,7 @@ int main(int argc, char* argv[]) {
 		if (screen.processEvents() == false) {
 			break;
 		}
+
 
 	}
 
